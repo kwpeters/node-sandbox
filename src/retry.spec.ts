9a70858e65fa44dc3be5f762ebb7d01551d0d76e
Kevin Peters <kwpeters@ra.rockwell.com>
2016-05-12 14:19:53 -0400

    Added retry.ts and retry.spec.ts.

--- src/retry.spec.ts
+++ src/retry.spec.ts
@@ -1,12 +1,38 @@
 ///<reference path="../typings/main.d.ts"/>
 
-
 import * as test from "tape";
 import {retry} from "./retry";
+import * as Promise from "bluebird";
 
 test("can call retry()",
     function (t: test.Test): void {
-        t.ok(retry());
-        t.end();
+        
+        const theFunc: () => Promise<void> = getFuncThatWillRejectNTimes(2);
+
+        retry(theFunc, 3)
+            .then(
+                () => {
+                    console.log("succeeded.");
+                    console.log("");
+                    t.end();
+                },
+                () => {
+                    console.log("failed.");
+                    console.log("");
+                }
+            );
+
+        
+        function getFuncThatWillRejectNTimes(numFailures: number): () => Promise<void> {
+            let numFailuresRemaining: number = numFailures;
+            
+            return () => {
+                if (numFailuresRemaining > 0) {
+                    --numFailuresRemaining;
+                    return Promise.reject(new Error("failure"));
+                }
+                return Promise.resolve();
+            };
+        }
     }
 );
--- src/retry.ts
+++ src/retry.ts
@@ -1,6 +1,28 @@
+///<reference path="../typings/main.d.ts"/>
 
+import * as Promise from "bluebird";
 
-export function retry(): boolean {
+export function retry<ResolveType>(theFunc:() => Promise<ResolveType>, numAttempts:number):Promise<ResolveType> {
     "use strict";
-    return true;
+    return new Promise(
+        (resolve: (value: ResolveType|Promise<ResolveType>) => void, reject: (err: any) => void) => {
+
+            theFunc().then(
+                (value: ResolveType) => {
+                    // The current iteration resolved.  Return the value to the client immediately.
+                    console.log("resolved.");
+                    resolve(value);
+                },
+                (err: any): void => {
+                    // The promise was rejected.
+                    console.log("rejected.");
+                    if (numAttempts - 1 === 0) {
+                        reject(err);
+                    } else {
+                        resolve(retry(theFunc, numAttempts - 1));
+                    }
+                }
+            );
+        }
+    );
 }
