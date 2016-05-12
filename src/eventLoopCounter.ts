600bcde895d669fd47d465fb81fef6d8cefcde5b
Kevin Peters <kwpeters@ra.rockwell.com>
2016-05-12 14:17:45 -0400

    Added eventLoopCounter.ts.

---
+++ src/eventLoopCounter.ts
@@ -0,0 +1,26 @@
+///<reference path="../typings/main.d.ts"/>
+
+/*
+ * This script demonstrates how you can throw a function's execution into a
+ * future node event loop.
+ */
+
+(function ():void {
+
+    let counter:number = 0;
+    let iterations:number = 1000;
+
+    const worker:() => void = () => {
+        console.log(++counter);
+
+        --iterations;
+        if (iterations > 0) {
+            setTimeout(worker);
+        }
+    };
+
+    worker();
+
+})();
+
+
