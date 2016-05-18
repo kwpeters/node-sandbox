52f9ff3dbb7a4fa2dc5f358d2787161d2aa32eaf
Kevin Peters <kwpeters@ra.rockwell.com>
2016-05-18 07:41:51 -0400

    Added getTimerPromise() and implemented promise retrying w/ exponential backoff.

--- src/retry.spec.ts
+++ src/retry.spec.ts
@@ -1,8 +1,31 @@
 ///<reference path="../typings/main.d.ts"/>
 
 import * as test from "tape";
-import {retry} from "./retry";
+import {retry, getTimerPromise, retryWhile} from "./retry";
 import * as Promise from "bluebird";
+
+test("getTimePromise()",
+    function (t: test.Test): void {
+
+        t.test("should resolve after the specified amount of time",
+            function (t: test.Test): void {
+                
+                const start: number = Date.now();
+                const delayMs: number = 200;
+                
+                getTimerPromise(delayMs, "foo").then(
+                    (val) => {
+                        t.equal(val, "foo");
+                        t.assert(Date.now() >= start + delayMs);
+                        t.end();
+                    }
+                );
+            }
+        );
+    }
+);
+
+
 
 test("retry()",
     function (t: test.Test): void {
@@ -80,3 +103,53 @@ function getFuncThatWillRejectNTimes<T, U>(numFailures: number, resolveValue: T,
     };
 }
 
+
+test("retryWhile()",
+    function (t: test.Test): void {
+        
+        t.test("will reject immediately if the while predicate says to stop trying",
+            function (t: test.Test): void {
+
+                const theFunc: () => Promise<string> = getFuncThatWillRejectNTimes(5, "bar", "rejected");
+
+                retryWhile(theFunc, () => false, 1000)
+                    .then(
+                        () => {
+                            t.fail("The promise should not have resolved.");
+                        },
+                        (err: any) => {
+                            t.equal(err, "rejected");
+                            t.end();
+                        }
+                    );
+            }
+        );
+
+
+        t.test("will eventually resolve if the while predicate always returns true",
+            function (t: test.Test): void {
+
+                const theFunc: () => Promise<string> = getFuncThatWillRejectNTimes(5, "bar", "rejected");
+
+                retryWhile(
+                    theFunc,
+                    (err: string) => {
+                        t.equal(err, "rejected");
+                        return err === "rejected";
+                    },
+                    1000)
+                    .then(
+                        (value) => {
+                            t.equal(value, "bar");
+                            t.end();
+                        },
+                        (err: any) => {
+                            t.fail("The promise should not have rejected.");
+                        }
+                    );
+            }
+        );
+
+
+    }
+);
--- src/retry.ts
+++ src/retry.ts
@@ -1,26 +1,88 @@
 ///<reference path="../typings/main.d.ts"/>
 
 import * as Promise from "bluebird";
+import * as _ from "lodash";
 
-export function retry<ResolveType>(theFunc:() => Promise<ResolveType>, numAttempts:number):Promise<ResolveType> {
+const BACKOFF_MULTIPLIER: number = 20;
+
+export function getTimerPromise<ResolveType>(
+    ms:             number,
+    resolveValue:  ResolveType
+): Promise<ResolveType> {
+    "use strict";
+
+    return new Promise(
+        (resolve: (resolveValue: ResolveType) => void, reject: (err: any) => void) => {
+            setTimeout(() => {
+                resolve(resolveValue);
+            }, ms);
+        }
+    );
+
+}
+
+
+export function retry<ResolveType>(theFunc:() => Promise<ResolveType>, maxNumAttempts:number):Promise<ResolveType> {
+    "use strict";
+    return retryWhileImpl(theFunc, () => true, maxNumAttempts, 0);
+}
+
+
+export function retryWhile<ResolveType>(
+    theFunc:() => Promise<ResolveType>,
+    whilePredicate: (err: any) => boolean,
+    maxNumAttempts:number)
+:Promise<ResolveType> {
+    "use strict";
+    return retryWhileImpl(theFunc, whilePredicate, maxNumAttempts, 0);
+}
+
+
+function retryWhileImpl<ResolveType>(
+    theFunc:         () => Promise<ResolveType>,
+    whilePredicate:  (err: any) => boolean,
+    maxNumAttempts:  number,
+    attemptsSoFar:   number
+): Promise<ResolveType> {
     "use strict";
     return new Promise(
         (resolve: (value: ResolveType|Promise<ResolveType>) => void, reject: (err: any) => void) => {
 
-            theFunc().then(
-                (value: ResolveType) => {
-                    // The current iteration resolved.  Return the value to the client immediately.
-                    resolve(value);
-                },
-                (err: any): void => {
-                    // The promise was rejected.
-                    --numAttempts;
-                    if (numAttempts === 0) {
-                        reject(err);
-                    } else {
-                        resolve(retry(theFunc, numAttempts));
+            ++attemptsSoFar;
+            theFunc()
+                .then(
+                    (value: ResolveType) => {
+                        // The current iteration resolved.  Return the value to the client immediately.
+                        resolve(value);
+                    },
+                    (err: any): void => {
+                        // The promise was rejected.
+                        if ((attemptsSoFar >= maxNumAttempts) || !whilePredicate(err)) {
+                            reject(err);
+                        } else {
+                            const backoffBaseMs: number = Math.pow(2, attemptsSoFar - 1) * BACKOFF_MULTIPLIER;
+
+                            // A random amount of time should be added to or
+                            // subtracted from the base so that multiple retries
+                            // don't get stacked on top of each other, making
+                            // the congestion even worse.  This random range
+                            // should be either the multiplier or 25% of the
+                            // calculated base, whichever is larger.
+
+                            const randomHalfRange: number = Math.max(BACKOFF_MULTIPLIER, 0.25 * backoffBaseMs);
+                            const randomMs: number = _.random(-1 * randomHalfRange, randomHalfRange);
+                            const delayMs: number = backoffBaseMs + randomMs;
+
+                            console.log("Failed. Queuing next attempt in " + backoffBaseMs + " + " + randomMs + " (" + delayMs + ") ms");
+                            const timerPromise: Promise<void> = getTimerPromise(delayMs, undefined);
+                            resolve(
+                                timerPromise
+                                    .then(() => {
+                                        return retryWhileImpl(theFunc, whilePredicate, maxNumAttempts, attemptsSoFar);
+                                    })
+                                );
+                        }
                     }
-                }
             );
         }
     );
